50b4a4f819a03e6e6ffce3cbe6dc0c65d4d8a3b5
DuckLuckBreakout <mrdivan07@gmail.com>
2019-04-06 21:59:23 +0300

    Add withrawal parser function

--- code/main.py
+++ code/main.py
@@ -2,13 +2,18 @@ import requests
 import json
 import time
 
-def fill_skins_for_withdrawal_list(skins_for_withdrawal):
+def fill_skins_for_withrawal_list(skins_for_withdrawal):
 
 	withrawal_file = open('../data/withdrawal_list.txt', 'r')
-	for lot in withrawal_file:
-		skins_for_withdrawal.append(lot)
 
-	return skins_for_withdrawal
+	skins_for_withrawal = []
+
+	for lot in withrawal_file:
+		skins_for_withrawal.append(lot)
+
+	withrawal_file.close()
+
+	return skins_for_withrawal
 
 
 def change_parser():
@@ -25,15 +30,35 @@ def change_parser():
 
 		for old, new in zip(skins_old_list, skins_new_list):
 			if old != new:
-				print('ЗАМЕЧЕНО ИЗМЕНЕНИЕ')
+				print('Noticed a change!')
 				print(old)
-				print(new)
-				print()
+				print(new,'\n')
 
 		skins_old_list = skins_new_list
 
-withdrawal_list = []
-withdrawal_list = fill_skins_for_withdrawal_list(withdrawal_list)
+def withrawal_parser(withrawal_list):
+
+	skins_old_json = requests.get('https://loot.farm/fullpriceDOTA.json')  
+	skins_old_list = json.loads(skins_old_json.content)
+
+	while True:
+
+		skins_json = requests.get('https://loot.farm/fullpriceDOTA.json')
+		skins_list = json.loads(skins_json.content)
+
+		for skin in skins_list:
+			if (skin['name'] in withrawal_list) and (skin['have'] != 0):
+				print('Noticed a desired skin!')
+				print(skin,'\n')
+				
+		time.sleep(60)
+
+
+
+
+withrawal_list = []
+withrawal_list = fill_skins_for_withrawal_list(withrawal_list)
+withrawal_parser(withrawal_list)
 
 
 
