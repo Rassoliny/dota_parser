3ade8fe92ac2f10bfa0026e04a746576af8d5c8f
DuckLuckBreakout <mrdivan07@gmail.com>
2019-04-07 13:28:16 +0300

    Add user notifications

--- code/main.py
+++ code/main.py
@@ -1,19 +1,20 @@
 import requests
 import json
 import time
+import os
 
-def fill_skins_for_withrawal_list(skins_for_withdrawal):
+def fill_skins_for_withdrawal_list(skins_for_withdrawal):
 
-	withrawal_file = open('../data/withdrawal_list.txt', 'r')
+	withdrawal_file = open('../data/withdrawal_list.txt', 'r')
 
-	skins_for_withrawal = []
+	skins_for_withdrawal = []
 
-	for lot in withrawal_file:
-		skins_for_withrawal.append(lot)
+	for lot in withdrawal_file:
+		skins_for_withdrawal.append(lot)
 
-	withrawal_file.close()
+	withdrawal_file.close()
 
-	return skins_for_withrawal
+	return skins_for_withdrawal
 
 
 def change_parser():
@@ -36,10 +37,13 @@ def change_parser():
 
 		skins_old_list = skins_new_list
 
-def withrawal_parser(withrawal_list):
+
+def withdrawal_parser(withdrawal_list):
 
 	skins_old_json = requests.get('https://loot.farm/fullpriceDOTA.json')  
 	skins_old_list = json.loads(skins_old_json.content)
+	ignore_list = []
+	ignore_names = []
 
 	while True:
 
@@ -47,18 +51,35 @@ def withrawal_parser(withrawal_list):
 		skins_list = json.loads(skins_json.content)
 
 		for skin in skins_list:
-			if (skin['name'] in withrawal_list) and (skin['have'] != 0):
-				print('Noticed a desired skin!')
-				print(skin,'\n')
+
+			if ((skin['name'] + '\n') in withdrawal_list) and \
+			   ((skin['name'] + '\n') not in ignore_names) and \
+				(skin['have'] != 0):
+
+				print('Noticed a desired skin!\n')
+				print(skin,'\n'*4)
+				ignore_list.append({'name' : skin['name'], 'time' : 5})
+				ignore_names.append(skin['name'] + '\n')
 				
+				os.system('say "Noticed a desired skin"')
+
 		time.sleep(60)
+		
+		for skin in ignore_list:
+			if skin['time']:
+				skin['time'] -= 1
+			else:
+				ignore_list.remove(skin)
+				ignore_names.remove(skin['name'] + '\n')
+
+		print(time.ctime(time.time()))
 
 
 
 
-withrawal_list = []
-withrawal_list = fill_skins_for_withrawal_list(withrawal_list)
-withrawal_parser(withrawal_list)
+withdrawal_list = []
+withdrawal_list = fill_skins_for_withdrawal_list(withdrawal_list)
+withdrawal_parser(withdrawal_list)
 
 
 
