b2c3e343c36ab36746963bc90c2afb7e9c50bb26
Rassoliny <dimo120@yandex.ru>
2019-04-10 23:36:18 +0300

    Change bot.py: receive handler, add and cls functions

--- code/bot.py
+++ code/bot.py
@@ -1,4 +1,4 @@
-from constants import TELE_TOKEN, CHANNEL_ID
+from constants import TELE_TOKEN
 import telebot
 
 
@@ -6,8 +6,29 @@ bot = telebot.TeleBot(TELE_TOKEN)
 
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(CHANNEL_ID, message.text)
+def receive_message(message):
+    """bot receive handler"""
+    command = message.text[:3]
+    skin_name = message.text[4:]
+
+    if command == 'cls':
+        # clean file
+        with open('../data/withdrawal_list.txt', 'w'):
+            pass
+        answer = 'File was cleared'
+
+    elif command == 'add':
+        # new record to file
+        with open('../data/withdrawal_list.txt', 'a') as f:
+            f.write(skin_name + '\n')
+        answer = '{} added to file'.format(skin_name)
+
+    else:
+        # no command received
+        answer = 'Did you forget to send command?'
+
+    # send answer to user
+    bot.send_message(message.chat.id, answer)
 
 
 if __name__ == '__main__':
